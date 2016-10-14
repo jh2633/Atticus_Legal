d19be677a78baab32e58bcf766e6eaa03a9cf7d8
Joseph <joseph.jr@gmail.com>
2016-10-14 23:08:04 +0100

    add contr and model for cases and users

---
+++ app/models/public.rb
@@ -0,0 +1,6 @@
+class Public < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
